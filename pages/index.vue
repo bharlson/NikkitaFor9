549cf226d1077e29edf0d1c50155240845db9c8d
Bjorn Harlson <bjorn.harlson@gmail.com>
2021-03-07 14:24:07 -0800

    Testing github repo

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
       <h1 class="title">
         Nikkita-Oliver-2021
       </h1>
+      <p>test</p>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
@@ -31,7 +32,7 @@
 export default {}
 </script>
 
-<style lang='scss'>
+<style>
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
@@ -39,42 +40,42 @@ export default {}
 */
 
 
-// .container {
-//   margin: 0 auto;
-//   min-height: 100vh;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   text-align: center;
-// }
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 
-// .title {
-//   font-family:
-//     'Quicksand',
-//     'Source Sans Pro',
-//     -apple-system,
-//     BlinkMacSystemFont,
-//     'Segoe UI',
-//     Roboto,
-//     'Helvetica Neue',
-//     Arial,
-//     sans-serif;
-//   display: block;
-//   font-weight: 300;
-//   font-size: 100px;
-//   color: #35495e;
-//   letter-spacing: 1px;
-// }
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
 
-// .subtitle {
-//   font-weight: 300;
-//   font-size: 42px;
-//   color: #526488;
-//   word-spacing: 5px;
-//   padding-bottom: 15px;
-// }
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
 
-// .links {
-//   padding-top: 15px;
-// }
+.links {
+  padding-top: 15px;
+}
 </style>
