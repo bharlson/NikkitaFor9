40828903b5f8b0d7ea37b8bd3e30d7b7e1391449
bharlson <bjorn.harlson@gmail.com>
2021-03-09 23:21:29 -0800

    DV page added

--- pages/Democracy-Vouchers/index.vue
+++ pages/Democracy-Vouchers/index.vue
@@ -1,13 +1,20 @@
 <template>
-  <p>test</p>
+<main>
+  <section-header text="Help Nikkita Qualify for Democracy Vouchers"></section-header>
+  <div class="p-4 md:px-24 bg-gray">
+    <iframe 
+    src="https://na4.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhBR7XCI2tDeuis6biwLE2H89wJQE9C-81sS6kDpTX8r-UpuhugYpXeYS28hyLBbd2o*&hosted=false" 
+    width="100%" height="100%" frameborder="0" 
+    style="border: 0; overflow: hidden; min-height: 500px; min-width: 300px;"></iframe>
+  </div>
+</main>
 </template>
 
 <script>
+import SectionHeader from '@/components/SectionHeader.vue'; 
 export default {
-    data(){
-        return{
-            
-        }
+    components: {
+        SectionHeader
     }
 }
 </script>
