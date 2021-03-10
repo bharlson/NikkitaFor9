b6886ff13c646db2eded5e32200ffe5fc8626c61
bharlson <bjorn.harlson@gmail.com>
2021-03-10 00:35:13 -0800

    Vision page + graycard component

---
+++ components/GrayCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="single-col-body">
+      <div class="p-2 pt-4 border-2 bg-white">
+        <h3 class="text-center">{{title}}</h3>
+      </div>
+      <div class="p-4 border-2 lg:px-12 border-t-0 bg-gray">
+        <p class="mb-4" v-html="text">
+           
+        </p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title:'',
+            text: ''
+        }
+    },
+    props: {
+        title: String,
+        text: String
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/Vision/index.vue
+++ pages/Vision/index.vue
@@ -1,33 +1,64 @@
 <template>
 <main>
   <section-header class="mt-0" text="Our Values"></section-header>
-    <div class="p-4 lg:px-40 grid grid-cols-1">
-      <h3 class="italic text-center">Basic needs and safety are intertwined</h3>
-      <p class="mb-4">When people’s basic needs are met, we build safety. Meeting basic needs is a baseline for community safety. Our city deserves better options than violent policing and mass incarceration as our only choices for public safety. The majority of what we call crime happens because people do not have their basic needs met. In order for us to create the safer city we imagine, we need affordable and social housing, equitable transportation, affordable childcare, fully funded schools with school counselors, restorative justice coordinators, and health services, more culturally responsive and accessible youth programs, health and sex education that teaches healthy relationships, accessible mental health supports, an array of community-based options for supporting domestic violence survivors and restroative and transformative responses for those who cause harm, civilianized 911, community-based drug user supports, and thriving wage employment opportunities.We must make investments in mobile mental health and crisis support teams immediately, so we can get the right care to people experiencing emergencies when they need it.</p>
-    
-    <h3 class="italic text-center">We must go beyond survival to a Seattle where we can all thrive</h3>
-    <p class="mb-4">We cannot thrive as a city, when our Black, Native, Brown, queer, trans, and disabled neighbors are being left behind. A rising tide may lift all boats, but we do not all have boats to catch the ride. Not to forget, a rising tide in the midst of a global climate catastrophe is a bad thing! Many in our City are being pushed out by the rising cost of housing, over policing and criminalization, and an overall lack of access to supportive services and opportunities to not simply meet basic needs but to ensure people can be self-determined and have control over their own lives. Equity and justice for all means that your race, gender, where you were born or the work you do does not prevent you from living a whole, healthy, thriving life. Thriving means that we have the right to joy, to health, to rest, to work, to balance, not just to getting by. We need economic, social, racial, and environmental justice, and a radical redistribution of wealth and political power to have the safer city of which we dream.</p>
-
-
-    <h3 class="italic text-center">Power comes from the people</h3>
-    <p class="mb-4"><b>Power comes from the people</b> and our policy making should too! We should develop our policy solutions together, following the lead of those most impacted and those actually doing the work on the ground. <b>The Solidarity Budget</b> is a great example of ground-up policy development as well as cross-movement coalition building. There is enough when we work together with shared values and vision.</p>
-
-    <h3 class="italic text-center">Community Listening Posts</h3>
-    <p class="mb-4">The goal of community listening posts is to come together to develop an evolving community platform that identifies the priorities and solutions of communities living and/or working in Seattle, and those who have been gentrified out of Seattle, who have been impacted by racism, ableism, poverty, police violence and other core areas where City policy can make a difference. This will change our platform and will change it for the better. As organizers and members of impacted communities we have identified some policy areas and possible solutions. None of this is set in stone--nor should it be. The greatest flaw of most electeds is the inability to change course when the community tells them to move in a different direction. The platform, policies, and solutions we develop together will chart the course for our next four years in office together continuing to build on the movements that have already brought us to this moment.</p>
-    </div>
+  <section id="basic-needs">
+    <gray-card
+      title="Basic needs and safety are intertwined"
+      text="When people’s basic needs are met, we build safety. Meeting basic needs is a baseline for community safety. Our city deserves better options than violent policing and mass incarceration as our only choices for public safety. The majority of what we call crime happens because people do not have their basic needs met. In order for us to create the safer city we imagine, we need affordable and social housing, equitable transportation, affordable childcare, fully funded schools with school counselors, restorative justice coordinators, and health services, more culturally responsive and accessible youth programs, health and sex education that teaches healthy relationships, accessible mental health supports, an array of community-based options for supporting domestic violence survivors and restroative and transformative responses for those who cause harm, civilianized 911, community-based drug user supports, and thriving wage employment opportunities.We must make investments in mobile mental health and crisis support teams immediately, so we can get the right care to people experiencing emergencies when they need it.">
+    </gray-card>
+  </section>
+  <section id="beyond-survival">
+    <gray-card
+      title="We must go beyond survival to a Seattle where we can all thrive"
+      text="We cannot thrive as a city, when our Black, Native, Brown, queer, trans, and disabled neighbors are being left behind. A rising tide may lift all boats, but we do not all have boats to catch the ride. Not to forget, a rising tide in the midst of a global climate catastrophe is a bad thing! Many in our City are being pushed out by the rising cost of housing, over policing and criminalization, and an overall lack of access to supportive services and opportunities to not simply meet basic needs but to ensure people can be self-determined and have control over their own lives. Equity and justice for all means that your race, gender, where you were born or the work you do does not prevent you from living a whole, healthy, thriving life. Thriving means that we have the right to joy, to health, to rest, to work, to balance, not just to getting by. We need economic, social, racial, and environmental justice, and a radical redistribution of wealth and political power to have the safer city of which we dream.">
+    </gray-card>
+  </section>
+  <section id="power">
+    <gray-card
+      title="Power comes from the people"
+      text="<b>Power comes from the people</b> and our policy making should too! We should develop our policy solutions together, following the lead of those most impacted and those actually doing the work on the ground. <b>The Solidarity Budget</b> is a great example of ground-up policy development as well as cross-movement coalition building. There is enough when we work together with shared values and vision.">
+    </gray-card>
+  </section>
+  <section id="listening-posts">
+    <gray-card
+      title="Community Listening Posts"
+      text="The goal of community listening posts is to come together to develop an evolving community platform that identifies the priorities and solutions of communities living and/or working in Seattle, and those who have been gentrified out of Seattle, who have been impacted by racism, ableism, poverty, police violence and other core areas where City policy can make a difference. This will change our platform and will change it for the better. As organizers and members of impacted communities we have identified some policy areas and possible solutions. None of this is set in stone--nor should it be. The greatest flaw of most electeds is the inability to change course when the community tells them to move in a different direction. The platform, policies, and solutions we develop together will chart the course for our next four years in office together continuing to build on the movements that have already brought us to this moment.">
+    </gray-card>
+  </section>
 </main>
 </template>
 
 <script>
+import GrayCard from '@/components/GrayCard';
+
 export default {
     data(){
         return{
 
         }
+    },
+    components:{
+      GrayCard
     }
 }
 </script>
 
 <style lang='scss'>
-
+#basic-needs{
+  background-size: cover;
+  background-image: url('@/assets/img/home/values_basic_needs_puzzle.jpg');
+}
+#beyond-survival{
+  background-size: cover;
+  background-image: url('@/assets/img/home/values_we_must_go_beyond_survival_spaceneedle.jpg');
+}
+#power{
+  background-size: cover;
+  background-image: url('@/assets/img/home/values_power_to_the_people_fist.jpg');
+  background-position:top left;
+}
+#listening-posts{
+  background-size: cover;
+  background-image: url('@/assets/img/home/values_community_listening_posts_mic.jpg');
+}
 </style>
