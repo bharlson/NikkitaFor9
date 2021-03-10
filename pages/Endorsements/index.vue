4d5863322fdf425cb1aca4dab97f56ef56e460be
bharlson <bjorn.harlson@gmail.com>
2021-03-10 02:28:52 -0800

    Added ind. endorsement disclaimer

--- pages/Endorsements/index.vue
+++ pages/Endorsements/index.vue
@@ -10,6 +10,9 @@
         :text="i.title"
         >
       </white-card>
+      <div class="col-span-2">
+        <p><span class="bold serif text-2xl">*</span> : Indicates an individual endorsement, which does not necessarily reflect the views of the endorser's associated organization</p>
+      </div>
     </div>
 
   </main>
@@ -49,11 +52,11 @@ export default {
                           },
 
                           {
-                            "name": "Sutapa Basu",
+                            "name": "Sutapa Basu *",
                             "title": "Executive Director, Alene Morris Women's Center at UW"
                           },
                           {
-                            "name": "Sean Goode",
+                            "name": "Sean Goode *",
                             "title": "Executive Director, Choose 180"
                           },
                           {
@@ -73,11 +76,11 @@ export default {
                             "title": ""
                           },
                           {
-                            "name": "Jose Luis Rodriguez ",
+                            "name": "Jose Luis Rodriguez *",
                             "title": "Owner of the Station"
                           },
                           {
-                            "name": "Leona Moore-Rodriguez",
+                            "name": "Leona Moore-Rodriguez *",
                             "title": "Owner of the Station"
                           },
                           {
@@ -85,96 +88,96 @@ export default {
                             "title": "Artist, Cultural Worker, Organizer"
                           },
                           {
-                            "name": "Matt Remle",
+                            "name": "Matt Remle *",
                             "title": "Co-Founder of Mazaska Talks"
                           },
                           {
-                            "name": "Jaelynn Scott",
+                            "name": "Jaelynn Scott *",
                             "title": "Executive Director of Lavendar Rights Project"
                           },
                           {
-                            "name": "Jesse Hagopian",
+                            "name": "Jesse Hagopian *",
                             "title": "Editor for Rethinking Schools, Orgnaizer with Black Lives Matter in School"
                           },
                           {
-                            "name": "Coach Dominique Davis",
+                            "name": "Coach Dominique Davis *",
                             "title": "CEO & Founder of Community Passageways"
                           },
                           {
-                            "name": "Ijeoma Oluo",
+                            "name": "Ijeoma Oluo *",
                             "title": "Author of \"So You Want To Talk About Race\" and \"Mediocre: The Dangerous Legacy of White Male America\""
                           },
                           {
-                            "name": "Katrina Johnson",
+                            "name": "Katrina Johnson *",
                             "title": "Families on the Frontline, Washington Coalition for Police Accountability"
                           },
 
                           {
-                            "name": "Robel Woldu",
+                            "name": "Robel Woldu *",
                             "title": "Owner of AthleticForm"
                           },
                           {
-                            "name": "Martina Kartman",
+                            "name": "Martina Kartman *",
                             "title": "Program Director & Co-Founder of Collective Justice"
                           },
                           {
-                            "name": "Sadé Smith, Attorney at Law",
+                            "name": "Sadé Smith, Attorney at Law *",
                             "title": "Dismantle, The Smith Law, LLC "
                           },
                           {
-                            "name": "Deaunte Damper",
+                            "name": "Deaunte Damper *",
                             "title": "Community Activist"
                           },
                           {
-                            "name": "Rev. Ellie VerGowe",
+                            "name": "Rev. Ellie VerGowe *",
                             "title": "MDiv\nOrdained minister with the Progressive Christian Alliance\nPastor of Outreach, Youth Children and Family at First Covenant Church\nLocal artist/voice teacher\nHarborview Spiritual Care Intern"
                           },
                           {
-                            "name": "Rev. Bianca Davis-Lovelace",
+                            "name": "Rev. Bianca Davis-Lovelace *",
                             "title": "Executive Director of REACH & Chair of the WA Poor People’s Campaign"
                           },
                           {
-                            "name": "Haydeé Lavariega (they/them)",
+                            "name": "Haydeé Lavariega (they/them) *",
                             "title": "Community Leader, Educator, and Activist"
                           },
                           {
-                            "name": "Rev. Dr. Kelle Brown",
+                            "name": "Rev. Dr. Kelle Brown *",
                             "title": "Senior Pastor, Seattle Plymouth Church UCC"
                           },
                           {
-                            "name": "The Rev. Cecilia Kingman",
+                            "name": "The Rev. Cecilia Kingman *",
                             "title": "Unitarian Universalist Parish Minister"
                           },
                           {
-                            "name": "Tahmina Watson",
+                            "name": "Tahmina Watson *",
                             "title": "Immigration Lawyer"
                           },
                           {
-                            "name": "Kamau Chege",
+                            "name": "Kamau Chege *",
                             "title": "Washington Community Alliance "
                           },
                           {
-                            "name": "KL Shannon",
+                            "name": "KL Shannon *",
                             "title": "Community Organizer"
                           },
                           {
-                            "name": "Shomya Tripathy",
+                            "name": "Shomya Tripathy *",
                             "title": "Director of Policy and Civic Engagement at Asian Counseling and Referal Services and Seattle South Asian for black lives"
                           },
                           {
-                            "name": "Lalita Uppala",
+                            "name": "Lalita Uppala *",
                             "title": "Community Advocate"
                           },
                           {
-                            "name": "Rabbi David Basior",
+                            "name": "Rabbi David Basior *",
                             "title": "Kadima Reconstructionist Community (for identification purposes only)"
                           },
                           {
-                            "name": "Alex Fayer",
+                            "name": "Alex Fayer *",
                             "title": "Community Organizer"
                           },
                           {
-                            "name": "Tyna Ek",
+                            "name": "Tyna Ek *",
                             "title": "Seattle Attorney"
                           }
                         ]
