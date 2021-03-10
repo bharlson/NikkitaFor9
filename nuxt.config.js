a1cb94195d6ad953e486277fc8bbf4ee6dc3c7ee
bharlson <bjorn.harlson@gmail.com>
2021-03-10 02:54:37 -0800

    more tweaks

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,15 +9,15 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1'  },
       { hid: 'description', name: 'description', content: 'Nikkita 4 Nine' },
-      { name: 'og:url', content: 'https://nikkita4nine.com' },
-      { name: 'og:image', content: '@/static/title-card.jpg' }
-      { name: 'og:type', content: 'website' },
-      { name: 'og:title', content: 'Nikkita Oliver for Seattle City Council' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
-      { hid: 'twitter:title', name: 'twitter:title', content:'Nikkita Oliver for Seattle City Council' },
-      { hid: 'twitter:card', name: 'twitter:card', content:'@/static/title-card.jpg' },
-      { hid: 'twitter:description', name: 'twitter:description', content: pkg.description }
+      { name: 'og:url', content: 'https://nikkita4nine.com' }
+      // { name: 'og:image', content: '@/static/title-card.jpg' }
+      // { name: 'og:type', content: 'website' },
+      // { name: 'og:title', content: 'Nikkita Oliver for Seattle City Council' },
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // { hid: 'description', name: 'description', content: pkg.description },
+      // { hid: 'twitter:title', name: 'twitter:title', content:'Nikkita Oliver for Seattle City Council' },
+      // { hid: 'twitter:card', name: 'twitter:card', content:'@/static/title-card.jpg' },
+      // { hid: 'twitter:description', name: 'twitter:description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/gif', href: '/favicon.gif' },
