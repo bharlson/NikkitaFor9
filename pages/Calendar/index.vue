886f56dc0f72f5ab1f3c2d98de19ba9f868deb46
bharlson <bjorn.harlson@gmail.com>
2021-06-25 14:01:07 -0700

    Experimenting with Google Cal integration

---
+++ pages/Calendar/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+      <section-header text="Calendar"></section-header>
+      <iframe src="https://calendar.google.com/calendar/embed?src=144oldmkrv4k4s6oi3gn9spkug%40group.calendar.google.com&ctz=America%2FLos_Angeles" 
+            style="
+            border: 0;
+            font-family:'Times New Roman';
+            " 
+            width="100%" 
+            
+            height="600" 
+            frameborder="0" 
+            scrolling="no">
+      </iframe>
+      <vue-google-calendar/>
+  </main>
+</template>
+
+<script>
+import VueGoogleCalendar from "vue-google-calendar/src/vue-google-calendar.vue"
+import SectionHeader from '@/components/SectionHeader.vue'; 
+// node_modules/vue-google-calendar/src/vue-google-calendar.vue
+export default {
+data(){  
+    return {
+        id: "144oldmkrv4k4s6oi3gn9spkug@group.calendar.google.com"
+        ,
+        // summary: "description ....",
+        // color: "#cd74e6",
+        dates: [
+            {
+                id: "144oldmkrv4k4s6oi3gn9spkug"
+                // ,
+                // summary: "event name",
+                // start: { dateTime: "2020-09-17T10:00:00-04:00" },
+                // end: { dateTime: "2020-09-17T11:00:00-04:00" }
+            }
+        ]
+    }
+},
+components: {
+    VueGoogleCalendar,
+    SectionHeader
+}
+    
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .cal-wrapper{
+        height:500px;
+    }
+</style>
