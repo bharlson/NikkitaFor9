927a79c8ec6fcf899a9528188cbc22a98c6ea32b
T-Allen <tariku.allen@gmail.com>
2021-07-09 20:09:49 -0700

    Added condition for subheading

--- components/SectionHeading.vue
+++ components/SectionHeading.vue
@@ -1,22 +1,22 @@
 <template>
-  <div 
-    v-editable="blok" 
-    class="section-header w-100 p-4 bg-white border-b-2 capitalize"
-    :class="blok.class"
-    >
-    
-      <h2 class="text-center pt-2"> {{blok.text}}</h2>
+  <div v-editable="blok" :class="blok.class">
+    <div class="section-header w-100 p-4 bg-white border-b-2 capitalize" v-if="!blok.subheading">
+        <h2 class="text-center pt-2"> {{blok.text}}</h2>
+    </div>
+    <div v-else>
+        <h3 class="my-6 text-center text-3xl">{{blok.text}}</h3>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
     props:{
-        // text: String,
+        subheading: false,
         blok: {
           type: Object,
           required: true
         }
-    }
+    },
 }
 </script>
