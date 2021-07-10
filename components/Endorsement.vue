a4ea4bb261b621298ea1bb2c89dcb1c0d9b0a1f0
T-Allen <tariku.allen@gmail.com>
2021-07-09 20:08:42 -0700

    Added image element for logo

--- components/Endorsement.vue
+++ components/Endorsement.vue
@@ -1,10 +1,11 @@
 <template>
     <article class="text-center bg-white" v-editable="blok">
       <div class="border-2">
-        <h3 class="p-2 text-center bg-white" v-if="blok.name">{{blok.name}}</h3>
+        <h4 class="p-2 text-center bg-white text-2xl" v-if="blok.name">{{blok.name}}</h4>
       </div>
       <div class="p-4 border-2 lg:px-12 border-t-0" v-if="blok.title">
         <p class="mb-4" v-if="blok.title">{{blok.title}}</p>
+        <img class="mx-auto max-h-24 object-contain" :src="blok.logo.filename" v-if="blok.logo"/>
       </div>
     </article>
 </template>
